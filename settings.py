8ef547b63ee1a8342f8ba50d21ab24d3445f8b22
Kolqa25 <109078291+Kolqa25@users.noreply.github.com>
2023-04-20 16:28:39 +0400

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,33 @@
+class AuthEmail:
+    email = 'qwerty@gmail.com'
+    password = 'tTeE1234'
+
+
+class AuthPhone:
+    phone = '9500674244'
+    password = 'tTeE1234'
+
+
+class AuthLogin:
+    login = 'rtkid_1672218272147'
+    password = 'tTeE1234'
+
+
+class InvalidData:
+    login = 'login_invalid'
+    password = "Qwerty12345"
+
+
+class SymbolData:
+    login = '@#####@'
+    password = "Qwerty12345"
+
+
+class KirillData:
+    login = 'кириллица'
+    password = "кириллица"
+
+
+class BIGData:
+    login = 'login'
+    password = "passw"
